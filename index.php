628c06a1073a8ec87e0c1219934738d6a73f5c7b
ChrisDCP <christopherdarielc@gmail.com>
2024-12-23 13:37:37 -0600

    ignore verify peer on curlsetopt

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
+
 const API_URL = "https://whenisthenextmcufilm.com/api";
 //inicializar una nueva sesion de cURL; ch = curl handler
 $ch = curl_init(API_URL);
 //indicar que queremos recibir el resultado de la peticion y no mostrarla en pantalla
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
 
 /* ejecutar la peticion y guardar el reultado */
 $result = curl_exec($ch);
 //obtener el json del resultado y guardarlo en un array asociativo
+
 $data = json_decode($result, true);
 
 curl_close($ch);
-
-if ($data === null) {
-    echo "Error al decodificar el JSON: " . json_last_error_msg();
-} else {
-    print_r($data);
-}
 ?>
 
 <head>
@@ -31,21 +29,21 @@ if ($data === null) {
 
 <main>
     <section>
-        <img src="<?= $data["poster-url"]; ?>" width="250" alt="poster de <?= $data["title"]; ?> "
+        <img src="<?= $data["poster_url"]; ?>" width="250" alt="poster de <?= $data["title"]; ?> "
         style= "border-radius: 16px">
     </section>
 
     <hgroup>
         <h3><?= $data["title"];?> se estrena en <?$data["days_until"];?> dias</h3>
         <p>Fecha de estreno: <?= $data["release_date"]?></p>
-        <p>La siguiente es: <?= $data["following_production"]?></p>
+        <p>La siguiente es: <?= $data["following_production"]["title"]?></p>
     </hgroup>
 
 </main>
 
 <style>
     :root{
-        color-scheme: light dark;
+        color-scheme: dark;
     }
 
     body{
